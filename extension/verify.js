18645bc10bc5506a4d3c520a20e0481f90adef24
kzzhang <kzhang2100@gmail.com>
2019-10-26 03:17:54 +0800

    set up basic server and extension

---
+++ extension/verify.js
@@ -0,0 +1,59 @@
+let server = "http://localhost:8080";
+let request = new XMLHttpRequest();
+let certificatesMap = new Map();
+
+console.log("Firefox Certificate Checker background script loaded!");
+
+let checkedWebsites = { urls: ['<all_urls>'] };
+let extraInfoSpec = ['blocking']; 
+
+browser.webRequest.onHeadersReceived.addListener(async function(details){
+  var requestId = details.requestId;
+
+  var securityInfo = await browser.webRequest.getSecurityInfo(requestId, {
+    certificateChain: true,
+    rawDER: false
+  });
+
+  certificatesMap.set(details.url, securityInfo)
+}, checkedWebsites, extraInfoSpec)
+
+async function checkTabSecurityInfo(url) {
+  setTimeout(function() {
+    let securityInfo = certificatesMap.get(url);
+    let certificatesList = securityInfo.certificates;
+    let certificate = certificatesList[0];
+
+    sendCert(url, certificate);
+  }, 2000);
+}
+
+async function sendCert(website, cert) {
+	request.open("POST", server);
+	request.setRequestHeader("Content-Type", "application/json");
+	request.overrideMimeType("application/json");
+	request.onload = function()
+	{
+	    if (request.status == 400) {
+	    	browser.notifications.create("Bad Certificate Detected!", {
+		    	"type": "basic",
+			    "title": "Bad Certificate Detected!",
+			    "message": "The certificate at URL " + request.response + " was different from what we found from our server."
+			});
+	    }
+	};
+	var bodyMap = {};
+	bodyMap["url"] = website;
+	bodyMap["certificate"] = cert;
+	request.send(JSON.stringify(bodyMap));
+}
+
+function handleUpdated(tabId, changeInfo, tabInfo) {
+  if (changeInfo.url) {
+    console.log("Tab: " + tabId +
+                " URL changed to " + changeInfo.url);
+    checkTabSecurityInfo(changeInfo.url);
+  }
+}
+
+browser.tabs.onUpdated.addListener(handleUpdated);
---
+++ server/index.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+const sslCertificate = require('get-ssl-certificate');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function removeQuotes(string) {
+	if (string.charAt(0) == "'" || string.charAt(0) == "\"") {
+		return string.substring(1, string.length - 1);
+	}
+	return string;
+}
+
+function equalsToObject(object) {
+	var output = {}
+	var tokens = object.split("=");
+	var currentKey = removeQuotes(tokens[0]);
+	for (i = 1; i < tokens.length; i++) {
+		var index = tokens[i].lastIndexOf(",");
+		var value;
+		if (i != tokens.length - 1) {
+			value = removeQuotes(tokens[i].substring(0, index));
+		} else {
+			value = removeQuotes(tokens[i]);
+		}
+		output[currentKey] = value;
+		if (i != tokens.length - 1) {
+			currentKey = removeQuotes(tokens[i].substring(index + 1, tokens[i].length));
+		}
+	}
+
+	return output;
+}
+
+function compareCertificates(incomingCertificate, certificate) {
+	var incomingCertificateSubject = equalsToObject(incomingCertificate.subject);
+	if (!_.isEqual(incomingCertificateSubject, certificate.subject)) {
+		return false;
+	}
+
+	var incomingCertificateIssuer = equalsToObject(incomingCertificate.issuer);
+	if (!_.isEqual(incomingCertificateIssuer, certificate.issuer)) {
+		return false;
+	}
+
+	var certificateStart = new Date(certificate.valid_from);
+	var certificateEnd = new Date(certificate.valid_to);
+	if (incomingCertificate.validity.start != certificateStart.valueOf() || incomingCertificate.validity.end != certificateEnd.valueOf()) {
+		return false;
+	}
+
+	if (incomingCertificate.fingerprint.sha1 != certificate.fingerprint) {
+		return false;
+	}
+
+	var incomingCertificateSerialTokens = incomingCertificate.serialNumber.split(":");
+	var incomingCertificateSerialNumber = "";
+	for (i = 0; i < incomingCertificateSerialTokens.length; i++) {
+		incomingCertificateSerialNumber += incomingCertificateSerialTokens[i];
+	}
+	if (incomingCertificateSerialNumber != certificate.serialNumber) {
+		return false;
+	}
+
+	return true;
+}
+
+app.post('/', function(req, res) {
+	console.log(req.body.url);
+
+	var incomingCertificate = req.body.certificate;
+	var url =  req.body.url.replace(/^https?\:\/\//i, "");
+	if (url.charAt(url.length - 1) == '/') {
+		url = url.substring(0, url.length - 1);
+	}
+
+	sslCertificate.get(url).then(function(certificate) {
+		if (compareCertificates(incomingCertificate, certificate)) {
+			console.log("Pass - " + url);
+			res.type('json')
+			res.status(200);
+			res.end();
+		} else {
+			console.log("Fail - " + url);
+			res.type('json')
+			res.status(400);
+			res.end(req.body.url);
+		}
+	});
+});
+
+app.listen(port, function() {
+	console.log('Listening on port: ' + port);
+});
